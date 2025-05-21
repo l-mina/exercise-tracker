fc2f469932dedba52181af74b3a159a1f270ea09
Luis M <150566024+l-mina@users.noreply.github.com>
2025-05-21 00:04:43 -0400

    changed router allowing scroll to top on new page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,36 @@ import Footer from "./components/Footer"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 
-import { Routes, Route, ScrollRestoration } from "react-router-dom"
+import { createBrowserRouter, RouterProvider, ScrollRestoration, Outlet } from "react-router-dom"
 import { useThemeStore } from "./store/useThemeStore"
+
+const HeaderLayout = () => (
+  <>
+    <header>
+      <Navbar />
+    </header>
+    <ScrollRestoration />
+    <Outlet />
+    <Footer />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <HeaderLayout />,
+    children: [
+      {
+        path:'/', 
+        element:<HomePage />
+      },
+      {
+        path:'/login',
+        element:<LoginPage />
+      },
+    ]
+  },
+  
+]);
 
 function App() {
 
@@ -13,16 +41,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme.theme}>
-
-      <Navbar />
-      
-      <Routes>
-        
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-      
-      <Footer />
+      <RouterProvider router={router} />
     </div>
   )
 }
